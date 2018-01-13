eb3be797af1c399b0bfcaaea1a47fb43b796ccf6
Glauber Modolo Cabral <glauber.sp@gmail.com>
2018-01-13 11:49:51 -0200

    Moodle funciona. Falta integrar NextCloud + Onlyoffice.

--- config/mysql/docker-entrypoint-initdb.d/onlyoffice-initdb.sql
+++ config/mysql/docker-entrypoint-initdb.d/onlyoffice-initdb.sql
@@ -1,13 +1,13 @@
 CREATE DATABASE IF NOT EXISTS onlyoffice CHARACTER SET "utf8" COLLATE "utf8_general_ci";
-CREATE DATABASE IF NOT EXISTS onlyoffice_mailserver CHARACTER SET "utf8" COLLATE "utf8_general_ci";
+CREATE DATABASE IF NOT EXISTS nextcloud CHARACTER SET "utf8" COLLATE "utf8_general_ci";
+CREATE DATABASE IF NOT EXISTS moodle CHARACTER SET "utf8mb4" COLLATE "utf8mb4_unicode_ci";
 
 CREATE USER IF NOT EXISTS 'onlyoffice_user'@'%' IDENTIFIED BY 'onlyoffice_pass';
-CREATE USER IF NOT EXISTS 'onlyoffice_mailserver_user'@'%' IDENTIFIED BY 'onlyoffice_mailserver_user_pass';
+CREATE USER IF NOT EXISTS 'nextcloud_user'@'%' IDENTIFIED BY 'nextcloud_pass';
+CREATE USER IF NOT EXISTS 'moodle_user'@'%' IDENTIFIED BY 'moodle_pass';
 
 GRANT ALL PRIVILEGES ON onlyoffice.* TO 'onlyoffice_user'@'%';
-GRANT ALL PRIVILEGES ON onlyoffice_mailserver.* TO 'onlyoffice_user'@'%';
-
-GRANT ALL PRIVILEGES ON onlyoffice.* TO 'onlyoffice_mailserver_user'@'%';
-GRANT ALL PRIVILEGES ON onlyoffice_mailserver.* TO 'onlyoffice_mailserver_user'@'%';
+GRANT ALL PRIVILEGES ON nextcloud.* TO 'nextcloud_user'@'%';
+GRANT ALL PRIVILEGES ON moodle.* TO 'moodle_user'@'%';
 
 FLUSH PRIVILEGES;
